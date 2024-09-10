fdb8a8312dd3b03692fe4808526759937b092a4b
Raul Veras <verasraul@gmail.com>
2024-09-10 11:43:28 -0400

    installing material ui and creating stylesheet

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import DrawIcon from '@mui/icons-material/Draw';
+
+
+function Header() {
+  return (
+    <header>
+        <DrawIcon />
+        <h1>ToDo List</h1>
+    </header>
+  )
+}
+
+export default Header
