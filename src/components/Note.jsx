20de89b89d7e058206fd5d8e8c81da6889a0ccb3
Raul Veras <verasraul@gmail.com>
2024-09-10 19:32:59 -0400

    creating note component

---
+++ src/components/Note.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import DeleteIcon from '@mui/icons-material/Delete';
+
+function Note(props) {
+  return (
+    <div className='note'>
+        Note.
+        <DeleteIcon />
+    </div>
+  )
+}
+
+export default Note;
