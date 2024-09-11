8c992ddaf6a4a8b3d1680b72570da38dabf4b5e6
Raul Veras <verasraul@gmail.com>
2024-09-10 21:10:02 -0400

    adding delete func

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -3,14 +3,14 @@ import DeleteIcon from '@mui/icons-material/Delete';
 
 function Note(props) {
 
-    function handleOnClick( {
+    function handleOnClick() {
         props.onDelete(props.id)
-    })
+    }
 
   return (
     <div className='note'>
         <h1>{props.title}</h1>
-        <p>props.content</p>
+        <p>{props.content}</p>
         <button onClick={handleOnClick}>
             <DeleteIcon />
         </button>
